51aa5833eea66d603880e36bd655bf1f29da0a45
saeed <saeid.kamyabi71@gmail.com>
2019-10-08 12:18:48 +0330

    update mongo module, improve movie model

--- go.mod
+++ go.mod
@@ -7,6 +7,6 @@ require (
 	github.com/go-ginger/helpers v1.0.1
 	github.com/go-ginger/logic v1.0.2
 	github.com/go-ginger/models v1.0.1
-	github.com/go-ginger/mongo v1.0.2
+	github.com/go-ginger/mongo v1.0.3
 	github.com/jinzhu/gorm v1.9.10
 )
--- project/models/movie.go
+++ project/models/movie.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Movie struct {
-	mongo.BaseModel
+	mongo.BaseModel `bson:"inline"`
 
-	Title       string    `bson:"title" json:"title,omitempty"`
-	Rate        float64   `bson:"rate" json:"rate,omitempty"`
-	ReleaseDate time.Time `bson:"release_date" json:"release_date,omitempty"`
-	Synopsis    string    `bson:"synopsis" json:"synopsis,omitempty"`
+	Title       string    `bson:"title,omitempty" json:"title,omitempty"`
+	Rate        float64   `bson:"rate,omitempty" json:"rate,omitempty"`
+	ReleaseDate time.Time `bson:"release_date,omitempty" json:"release_date,omitempty"`
+	Synopsis    string    `bson:"synopsis,omitempty" json:"synopsis,omitempty"`
 }
